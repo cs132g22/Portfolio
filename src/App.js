526b53f5e72a79c0cd185234dbb77210276be0a9
anagramiccoder <jdvidad31@gmail.com>
2023-03-02 09:38:38 +0800

    added text to sections

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import mlogo from './mainlogo.png';
 import './App.css';
 import './fonts/tiltwarp.ttf';
 import motivate from './motivate.png';
+import prob from './problem.png';
+import sol from './solution.png';
 function App() {
   return (
     <div className="App">
@@ -18,22 +20,22 @@ function App() {
             <Navbar.Brand href="#home">Group 22</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Item>
-                <Nav.Link href="/Overview">Top</Nav.Link>
+              <Nav.Link href="#section1">Top</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Overview">Overview</Nav.Link>
+                <Nav.Link href="#section2">Overview</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Data</Nav.Link>
+                <Nav.Link href="#section3">Data</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Methods</Nav.Link>
+                <Nav.Link href="#section4">Methods</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Results</Nav.Link>
+                <Nav.Link href="#section5">Results</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Team</Nav.Link>
+                <Nav.Link href="#section6">Team</Nav.Link>
               </Nav.Item>
             </Nav>
           </Container>
@@ -48,22 +50,22 @@ function App() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
         <Nav.Item>
-                <Nav.Link href="/Overview">Top</Nav.Link>
+              <Nav.Link href="#section1">Top</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Overview">Overview</Nav.Link>
+                <Nav.Link href="#section2">Overview</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Data</Nav.Link>
+                <Nav.Link href="#section3">Data</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Methods</Nav.Link>
+                <Nav.Link href="#section4">Methods</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Results</Nav.Link>
+                <Nav.Link href="#section5">Results</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/Data">Team</Nav.Link>
+                <Nav.Link href="#section6">Team</Nav.Link>
               </Nav.Item>
         </Nav>
         </Navbar.Collapse>
@@ -71,7 +73,7 @@ function App() {
       </MediaQuery>
       </div>
       <div className='body'>
-        <div className="bcontainer top1">
+        <div className="bcontainer top1" id='section1'>
           <div className='left'>
                 <img src={mlogo} className="leftimg" alt="vectorjuice on Freepik"/>
           </div>
@@ -91,7 +93,7 @@ function App() {
           
           </div>
         </div>
-        <div className="bcontainer"><h1>This is What it is all About</h1><p>so project namin ganito ganyan</p>
+        <div className="bcontainer" id='section2'><h1>This is What it is all About</h1><p>so project namin ganito ganyan</p>
           <div className="cards"><div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front">
@@ -99,45 +101,74 @@ function App() {
                   Motivation
               </div>
               <div class="flip-card-back">
-                <h1>John Doe</h1>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <p>Pasensya, di kami motivated, grades lang to (jk)</p>
               </div>
             </div>
           </div>
           <div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front">
-              <img src={motivate} alt=''/><br/>
-                  Motivation
+              <img src={prob} alt=''/><br/>
+                  Problem
               </div>
               <div class="flip-card-back">
-                <h1>John Doe</h1>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <p>
+                  Here is our problem, maraming fake news ganun,
+                  di ko sure bat naging problema pa namin eh
+                </p>
               </div>
             </div>
           </div>
           <div class="flip-card">
             <div class="flip-card-inner">
               <div class="flip-card-front">
-              <img src={motivate} alt=''/><br/>
-                  Motivation
+              <img src={sol} alt=''/><br/>
+                  Solution
               </div>
               <div class="flip-card-back">
-                <h1>John Doe</h1>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
+                <p> Solusyon eh mag track lang ng mga tweets ganun, diba.
+                  mas mabilis kasi kumuha ng eto lang tas ganun, hanap lang ng data,
+                </p>
               </div>
             </div>
           </div>
           </div>
            
         </div>
-        <div className="bcontainer">3</div>
-        <div className="bcontainer">4</div>
-        <div className="bcontainer">5</div>
-        <div className="bcontainer">6</div>
+        <div className="bcontainer" id='section3'>
+          <h1>So Naging minero kami ng datus</h1>
+          <div className='viewer'>
+            <div class="section">
+              <div className='left'>
+                left
+              </div>
+              <div className='right'>right</div>
+            </div>
+            <div class="section">
+              <div className='left'>
+                TOPIC
+              </div>
+              <div className='right'>Diba undecidable pa haha di pa alam kung ano final topic</div>
+            </div>
+            <div class="section">
+              <div className='left'>
+                KEYWORDS
+              </div>
+              <div className='right'>maissue kasi, kaya maraming KEYWORDS</div>
+            </div>
+            <div class="section">
+              <div className='left'>
+                TOOLS
+              </div>
+              <div className='right'>mga martilyo ganun</div>
+            </div>
+          </div> 
+        </div>
+        <div className="bcontainer" id='section4'><h1>Ganito naman Ginawa namin para makuha yung datus</h1>
+        <p> yep wala pa, wala pa kami nagagawa</p></div>
+        <div className="bcontainer" id='section5'><h1> Ang resulta</h1><p> Syempre wala pa wala pa nga naggawa diba</p></div>
+        <div className="bcontainer" id='section6'><h1> may tanong ba sa amin?</h1><p>
+          di pa namn masasagot kasi wala rin kami alam diyan</p></div>
       </div>
     </div>
   );
