04503fdd5df3d5a1694bd749b93cd50e2a4b96b3
reannu123 <reannumon123@gmail.com>
2023-05-17 11:23:15 +0800

    workaround fix for graph data not showing properly

--- src/NewSection/Data.jsx
+++ src/NewSection/Data.jsx
@@ -1,28 +1,51 @@
 import { Modal } from "../components/Modal";
 import { motion } from "framer-motion";
-import React, {useState} from "react";
-import dataimg from "../components/data.png"
+import React, { useState } from "react";
+import dataimg from "../components/data.png";
 import "./section.css";
 import BarChart from "./barchart";
-function Data() {
-const [openModal, setOpenModal] = useState(false);
-const handleClose=()=>setOpenModal(false);
+import { Bar } from "react-chartjs-2";
 
-  return (<>
-  <center>
-    <motion.button 
-    whileHover={{ scale: [null, 1.3, 1.15] }}
-    transition={{ duration: 0.15}}
-    onClick={()=>{setOpenModal(true)}} className="but"><img src={dataimg} alt='data'/><br/>Data</motion.button></center>
-    <Modal open={openModal} close={handleClose}>
-    <div className="title">
-        <h2>Data</h2>
-      </div>
+function Data() {
+  const [openModal, setOpenModal] = useState(false);
+  const handleClose = () => setOpenModal(false);
+  const labels = ["January", "February", "March", "April", "May", "June"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+  return (
+    <>
       <center>
-        <BarChart/>
-        testing
+        <motion.button
+          whileHover={{ scale: [null, 1.3, 1.15] }}
+          transition={{ duration: 0.15 }}
+          onClick={() => {
+            setOpenModal(true);
+          }}
+          className="but"
+        >
+          <img src={dataimg} alt="data" />
+          <br />
+          Data
+        </motion.button>
       </center>
-    </Modal>
+      <Modal open={openModal} close={handleClose}>
+        <div className="title">
+          <h2>Data</h2>
+        </div>
+        <center>
+          <BarChart data={data} options={{ IndexAxis: "y" }} />
+        </center>
+      </Modal>
     </>
   );
 }
--- src/NewSection/barchart.jsx
+++ src/NewSection/barchart.jsx
@@ -2,22 +2,11 @@ import React from "react";
 import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 
-const BarChart = () => {
-  const labels = ["January", "February", "March", "April", "May", "June"];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
-      },
-    ],
-  };
+const BarChart = (props) => {
   return (
     <div>
-      <Bar data={data} />
+      // eslint-disable-next-line react/prop-types
+      <Bar data={props.data} options={props.options} />
     </div>
   );
 };
