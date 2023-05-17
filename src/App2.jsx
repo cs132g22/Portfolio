8727e5f189f9656551ea6a5f93e15c8fdfe9aed2
reannu123 <reannumon123@gmail.com>
2023-05-17 22:32:26 +0800

    Removed media query for now

--- src/App2.jsx
+++ src/App2.jsx
@@ -10,12 +10,14 @@ import Results from "./NewSection/Results";
 import Data from "./NewSection/Data";
 function App() {
   return (
-    <MediaQuery orientation="landscape" min-width={720}>
-      <div className="section">
-        <div className="upper">
-          <div className="box"><h1>Hi, We are Group 22</h1  ><h5>
-              <strong>Data Science Team</strong>
-            
+    // <MediaQuery orientation="landscape" min-width={720}>
+    <div className="section">
+      <div className="upper">
+        <div className="box">
+          <h1>Hi, We are Group 22</h1>
+          <h5>
+            <strong>Data Science Team</strong>
+
             <p>
               Reannu Instrella,
               <br />
@@ -24,37 +26,41 @@ function App() {
               John David Vidad, WFW
               <br />
             </p>
-            </h5></div>
-          <div className="box"><img src={topimg} alt="terrorists from designs.ai" /></div>
-          <div className="box">
-            <p>
-              This is our Project titled,
-              <b>The Anti Terror Bill mis/disinformation</b>. Considering how mis/disinformation is chronic in our daily lives, 
-              it is undeniable that it is also used on bills being read. We therefore ventured through twitter to find tweets related to Anti Terror Bill, especially during its passing, that contains Mis/Disinformation.
-
-            </p>
-            
-          </div>
+          </h5>
         </div>
-        <div className="lower">
-          <section>
-          <Overview/>
-          </section>
-          <section>
-          <Data/>
-          </section>
-          <section>
-          <Methodology/>
-          </section>
-          <section>
-          <Results/>
-          </section>
-          <section>
-          <About/>
-          </section>
+        <div className="box">
+          <img src={topimg} alt="terrorists from designs.ai" />
+        </div>
+        <div className="box">
+          <p>
+            This is our Project titled,
+            <b>The Anti Terror Bill mis/disinformation</b>. Considering how
+            mis/disinformation is chronic in our daily lives, it is undeniable
+            that it is also used on bills being read. We therefore ventured
+            through twitter to find tweets related to Anti Terror Bill,
+            especially during its passing, that contains Mis/Disinformation.
+          </p>
         </div>
       </div>
-    </MediaQuery>
+      <div className="lower">
+        <section>
+          <Overview />
+        </section>
+        <section>
+          <Data />
+        </section>
+        <section>
+          <Methodology />
+        </section>
+        <section>
+          <Results />
+        </section>
+        <section>
+          <About />
+        </section>
+      </div>
+    </div>
+    // </MediaQuery>
   );
 }
 
